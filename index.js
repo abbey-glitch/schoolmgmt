54ffcc20a8eb88cd5a1f41bd08d3cee9048b3afc
abbey-glitch <abiodunonaolapi@gmail.com>
2024-09-20 14:14:38 +0100

    multer middleware for image upload

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const session = require("express-session");
 const path = require("path");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
+const multer  = require('multer')
+// const cookieParser = require("cookie-parser");
 const bcrypt = require("bcrypt");
 const dotenv = require("dotenv");
 
@@ -22,10 +23,10 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
-app.use(cookieParser());
+// app.use(cookieParser());
 app.use(session({ secret: "secret", 
-    resave: true, 
-    saveUninitialized: true 
+    resave: false, 
+    saveUninitialized: false 
 }));
 // create the home route
 app.get("/", (req, res) => {
@@ -52,18 +53,59 @@ app.post("/register", async (req, res) => {
     }
 })
 // 
+app.get("/login", (req, res) => {
+    res.render("login");
+})
 // login route
-app.post("/login", async(req, res)=>{
-    fname = req.body.fname
-    password = req.body.password
-    profile = {
-        fname: fname,
-        password: password
+app.post("/login", (req, res)=>{
+    const email = req.body.email
+    const password = req.body.password
+    const profile = {
+        email:email,
+        password:password
     }
     req.session.user = profile
-    res.send("Login successful")
+    res.send("user login")
 })
 
+// create profile route
+app.get("/profile", (req, res) => {
+    const user = req.session.user
+    if(user){
+        res.send(`welcome ${user['email']}`)
+    }else{
+        res.send("please login first")
+    }
+
+    // res.render("profile");
+})
+const storage = multer.diskStorage({
+    destination:function(req, file, cb){
+        cb(null, path.join(__dirname, "public/content"));
+
+    },
+    filename:function(req, file, cb){
+        cb(null, file.originalname);
+    }
+})
+const upload = multer({storage:storage})
+// display form for upload
+app.get("/form/upload", (req, res)=>{
+    res.render("upform")
+})
+
+// process the form upload
+app.post("/form/upload", upload.single("image"), (req, res)=>{
+    const imgname = req.file.path
+    const imgdesc = req.body.description
+    res.send("image uploaded successfully")
+
+})
+// logout
+app.get("/logout", (req, res)=>{
+    session.clear
+    res.send("user logout successfully")
+})
 // make server connection
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
